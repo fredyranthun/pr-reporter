83950ae9fc16b18a3b9d50a765931fd233aa8a46
Fredy Ranthun <fredyranthun@gmail.com>
2026-09-22 15:15:01 -0300

    build: set up Go command for T02

---
+++ cmd/pr-report/main.go
@@ -0,0 +1,12 @@
+// Command pr-report will report open pull requests across GitHub repositories.
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	fmt.Fprintln(os.Stderr, "pr-report: report collection is not implemented yet")
+	os.Exit(1)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/fredyranthun/pr-reporter
+
+go 1.25.0
